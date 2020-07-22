46aa9c291b0abb364810c9d3c7902e65836f0c94
tianrui <haitongjie@qq.com>
2020-07-22 18:57:47 +0800

    [practice9][Ray] feature:complete practice9

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,71 @@
 package practice09;
 
 public class Person {
+    String name;
+    Integer age;
+    Integer id;
+
+    public Person(Integer id, String name, Integer age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        Person person = (Person) obj;
+        if (person == null) {
+            return false;
+        }
+        return person.id.equals(this.id);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.id;
+//     result = prime * result + age;  
+//     result = prime * result + ((uname == null) ? 0 : uname.hashCode());  
+//     result = prime * result + ((sex == null) ? 0 : sex.hashCode());  
+//     result = prime * result + ((city == null) ? 0 : city.hashCode());
+        return result;
+
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,42 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    Klass klass;
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(String name, Integer age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+
+        return this.klass != null ? super.introduce() + " I am a Teacher. I teach Class " + this.klass.number + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    public String introduceWith(Student jerry) {
+        return jerry.klass.number.equals(this.klass.number) ? super.introduce() + " I am a Teacher. I teach Jerry." : super.introduce() + " I am a Teacher. I don't teach Jerry.";
+    }
 }
