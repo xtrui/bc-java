d77b96dcb0744b9172337af614c2759f281eea31
Ray <ray.wen@oocl.com>
2020-07-22 16:14:55 +0800

    [practice6][Ray] feature:complete practice6.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    Integer klass;
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+
+        return this.klass != null ? super.introduce() + " I am a Teacher. I teach Class " + this.klass + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
 }
