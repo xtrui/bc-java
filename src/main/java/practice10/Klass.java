69dcc635ca43d8038d43c1f358540cac7c5719df
tianrui <haitongjie@qq.com>
2020-07-22 19:57:29 +0800

    [practice10][Ray] feature:complete practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,55 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    Integer number;
+    Student leader;
+    List<Student> students;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.students = new ArrayList<>();
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (!student.klass.students.contains(student)) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            setLeader(student);
+        }
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
+
+    public boolean isIn(Student student) {
+        return students.contains(student);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,46 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    LinkedList<Klass> classes;
+
+    public Teacher(Integer id, String name, Integer age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+
+
+    public boolean isTeaching(Student student) {
+        return this.classes.contains(student.klass);
+    }
+
+    public String introduceWith(Student student) {
+        return isTeaching(student) ? super.introduce() + " I am a Teacher. I teach Jerry." : super.introduce() + " I am a Teacher. I don't teach Jerry.";
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder classesStr = new StringBuilder();
+        if (this.classes != null) {
+            for (Klass klass : this.classes) {
+                classesStr.append(klass.number);
+                classesStr.append(", ");
+            }
+            classesStr.delete(classesStr.length() - 2, classesStr.length());
+        }
+        return this.classes != null ? super.introduce() + " I am a Teacher. I teach Class " + classesStr + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
 }
