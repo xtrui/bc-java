8a0a446ac8ceab5665295bdeaacb490ff855a9cf
Ray <ray.wen@oocl.com>
2020-07-22 16:04:18 +0800

    [practice5][Ray] feature:complete practice5.

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,32 @@
 package practice05;
 
 public class Person {
+    String name;
+    Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
 }
--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,28 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, Integer age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
+
+    public Student(String name, Integer age) {
+        super(name, age);
+    }
 }
